2d38c8e202caae00f221d9ffafdd1689ab983389
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-11-27 00:19:55 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinit::Application.config.secret_key_base = 'dd17044820f4c7e53d1c06c95361cb4b6890263a2c84815620172a7f4ffdd2a1761641e04987815fc534b16fc83d70c15b110a3503c49d3714bce09d3254c540'
